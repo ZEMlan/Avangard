5e7bf0ff612d73632f8e7d2b36cafa0a0169b491
regiri <goparegiri@gmail.com>
2020-01-22 13:55:06 +0300

    fixed menu + hero animation

--- Assets/scripts/UIScripts/Play.cs
+++ Assets/scripts/UIScripts/Play.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Play : MonoBehaviour {
+    public int ProgrammngLanguage;
     public void BAMSPressed()
     {
+        ProgrammngLanguage = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void CSPressed()
     {
+        ProgrammngLanguage = 2;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void JavaPressed()
     {
+        ProgrammngLanguage = 3;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
---
+++ Assets/scripts/mapScripts/AnimatinosScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AnimatinosScript : MonoBehaviour
+{
+    private Animator anim;
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+    void Update() {
+        if (heromove.is_moving) {
+            anim.SetBool("can_move", true);
+        }
+        else {
+            anim.SetBool("can_move", false);
+        }
+        if (Input.GetAxis("Horizontal") > 0) {
+            anim.SetBool("right", true);
+            anim.SetBool("left", false);
+        }
+        else if (Input.GetAxis("Horizontal") < 0) {
+            anim.SetBool("left", true);
+            anim.SetBool("right", false);
+        }
+        else {
+            anim.SetBool("left", false);
+            anim.SetBool("right", false);
+        }
+        if (Input.GetAxis("Vertical") < 0) {
+            anim.SetBool("forward", true);
+            anim.SetBool("back", false);
+        }
+        else if (Input.GetAxis("Vertical") > 0) {
+            anim.SetBool("back", true);
+            anim.SetBool("forward", false);
+        }
+        else {
+            anim.SetBool("forward", false);
+            anim.SetBool("back", false);
+        }
+    }
+}
