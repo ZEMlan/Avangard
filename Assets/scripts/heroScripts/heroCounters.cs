31d310b587fabc0ec09e981b9a4d19ee0af34336
Egor <khramovegor@gmail.com>
2020-01-23 23:36:33 +0300

    Added xml tasks

--- Assets/scripts/heroScripts/heroCounters.cs
+++ Assets/scripts/heroScripts/heroCounters.cs
@@ -7,7 +7,7 @@ public class heroCounters : MonoBehaviour
     public enum Language { Csharp, Java, Python };
 
     public static int dialogCounter = 1;
-    public static Language lang = Language.Python;
+    public static Language lang = Language.Csharp;
     public static int score = 0;
     public static int attempts = 0;
     public System.DateTime StartTime;
---
+++ Assets/scripts/questScripts/Robot2Task.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot2Task : MonoBehaviour
+{
+    private bool taskComplete = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && !taskComplete && heroCounters.dialogCounter == 5 && tag == "Robot2")
+        {
+            FindObjectOfType<questObjScr>().CompleteTask();
+            taskComplete = true;
+        }
+    }
+}
